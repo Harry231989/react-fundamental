e48b092dec3fdb3d494fbf7ae8ce4fa987ca07fd
Harry Fosu <harry23adjei@yahoo.com>
2023-04-20 13:49:55 +0200

    finished first_project and added tour project

--- 01-birthday-buddy/starter/src/App.jsx
+++ 01-birthday-buddy/starter/src/App.jsx
@@ -1,4 +1,29 @@
+import { useState } from "react";
+import data from "./data"
+import List from "./List";
+
 const App = () => {
-  return <h2>Birthday Reminder - Starter</h2>;
+ const [people, setPeople] = useState(data);
+
+ const handleBtn = () => {
+  setPeople([])
+ }
+
+ const resetBtn = () => {
+  setPeople(data)
+ }
+
+  return (
+    <main>
+      <section className='container'>
+        <h3> {people.length} birthdays today </h3>
+        <List people={people} />
+        <button onClick={handleBtn}  type="button" className="btn btn-block">Clear Button</button>
+        <button onClick={resetBtn}>Reset Btn</button>
+      </section>
+    </main>
+  );
+  
+  
 };
 export default App;
